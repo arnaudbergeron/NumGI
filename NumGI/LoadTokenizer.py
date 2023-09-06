da0c6ac66dc4806a0d172d8bd48c4437ab31f2b9
Arnaud Bergeron <bergerona62@gmail.com>
2023-09-06 17:37:33 -0400

    added LoaderTokenizer

---
+++ NumGI/LoadTokenizer.py
@@ -0,0 +1,40 @@
+from EquationTokenizer import EquationTokenizer, defaultTokenizer
+from DatasetTokenizer import DatasetTokenizer
+import torch
+
+class LoadTokenizer(DatasetTokenizer):
+    """This is the tokenizer used when loading data from files.
+
+    Args:
+        DatasetTokenizer (_type_): _description_
+    """
+    def __init__(self, x_files, y_files):
+        default_tokenized_x = []
+        default_tokenized_y = []
+
+        tempTokenizer = DatasetTokenizer([['1','2']], [['1','2']], True)
+
+        #load files
+        max_length = 0
+        for (x_file, y_file) in zip(x_files, y_files):
+            _torch_x = torch.load(x_file)
+            _torch_y = torch.load(y_file)
+            default_tokenized_x.append(_torch_x)
+            default_tokenized_y.append(_torch_y)
+            max_length = max(max_length, _torch_x.shape[1])
+            max_length = max(max_length, _torch_y.shape[1])
+
+        for idx, (x, y) in enumerate(zip(default_tokenized_x, default_tokenized_y)):
+            default_tokenized_x[idx] = tempTokenizer.tensorize_and_pad_by_len(x, max_length)
+            default_tokenized_y[idx] = tempTokenizer.tensorize_and_pad_by_len(y, max_length)
+
+        default_combined_x_torch = torch.cat(default_tokenized_x,axis=0)
+        default_combined_y_torch = torch.cat(default_tokenized_x,axis=0)
+
+        new_x = [tempTokenizer.tokens_to_list(i) for i in default_combined_x_torch.tolist()]
+        new_y = [tempTokenizer.tokens_to_list(i) for i in default_combined_y_torch.tolist()]
+
+        super().__init__(new_x, new_y, False)
+
+        
+        
