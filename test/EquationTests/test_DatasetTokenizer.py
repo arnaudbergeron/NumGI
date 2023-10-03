3fcd228f1c01f35769a9da5136dd62de90cb4058
Arnaud Bergeron <bergerona62@gmail.com>
2023-10-03 18:03:59 -0400

    modified some texts for new changes

--- test/EquationTests/test_DatasetTokenizer.py
+++ test/EquationTests/test_DatasetTokenizer.py
@@ -7,15 +7,25 @@ from NumGI.DatasetTokenizer import DatasetTokenizer
 
 def test_DatasetTokenizer():
     sp_x = sp.symbols("x")
-    eq = sp.Eq(sp.sin(sp_x), sp.symbols("y"))
-    sol = sp.Eq(sp.Function("f")(sp_x), 1)
+    sp_f = sp.Function("f")(sp_x)
+    eq = sp.Eq(sp_f, sp_x)
+    sol = sp.Eq(sp_f, sp_x)
 
-    tokenizer = DatasetTokenizer([eq, eq], [sol, sol])
+    tokenizer = DatasetTokenizer([eq, eq], [sol, sol], True, True)
+
+    eq = tokenizer.x_tokenized[0].tolist()
+    sol = tokenizer.y_tokenized[0].tolist()
+
+    eq = tokenizer.tokens_to_sympy(eq)
+    sol = tokenizer.tokens_to_sympy(sol)
+
+    assert (eq.replace(sol.lhs, sol.rhs)).doit()
 
     assert tokenizer is not None
     assert tokenizer.x_tokenized is not None
     assert tokenizer.y_tokenized is not None
-    assert tokenizer.max_length is not None
+    assert tokenizer.max_length_x is not None
+    assert tokenizer.max_length_y is not None
 
     tokenizer.split(0.5)
 
